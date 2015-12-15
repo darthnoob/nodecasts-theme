34dc56dd19c6ae5bcad723a14ce12906e336cf40
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-15 00:51:58 -0500

    minor CSS tweaks

--- homepage.php
+++ homepage.php
@@ -17,15 +17,13 @@ get_header(); ?>
             <?php foreach (get_categories($args) as $key => $cat) : ?>
             <?php if ($key == 3) { echo '</div><div class="row">'; } ?>
             <div class="col-md-4">
-                <div class="panel panel-success">
+                <div class="panel panel-default">
                     <h5 class="panel-heading">
                         <?php echo $cat->cat_name; ?>
                     </h5>
                     <div class="panel-body path-body">
                         <a href="<?php echo get_category_link($cat->term_id); ?>">
                             <?php echo $cat->description; ?>
-                        </a>
-                        <a href="<?php echo get_category_link($cat->term_id); ?>">
                             <img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" width="100"/>
                         </a>
                     </div>
@@ -47,7 +45,7 @@ get_header(); ?>
                         <div class="thumbnail">
                             <img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" />
                             <div class="caption"> 
-                                <h5 class="text-center"><?php echo $cat->cat_name; ?></h5>
+                                <h3 class="text-center"><?php echo $cat->cat_name; ?></h3>
                                 <p><?php echo $cat->description; ?></p>
                             </div>
                         </div>
