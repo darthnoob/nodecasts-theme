b965435ad5e505dc1762a62ff7cbac918c7849dd
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-15 00:20:57 -0500

    fixed homepage

--- homepage.php
+++ homepage.php
@@ -14,22 +14,25 @@ get_header(); ?>
 <div class="row">
     <div class="col-md-10 col-md-offset-1 text-center">
         <div class="row">
-        <?php foreach (get_categories($args) as $key => $cat) : ?>
-        <?php if ($key == 3) { echo '</div><div class="row">'; } ?>
-        <div class="col-md-4">
-            <div class="panel panel-success">
-                <h5 class="panel-heading">
-                    <?php echo $cat->cat_name; ?>
-                </h5>
-                <div class="panel-body path-body">
-                    <a href="<?php echo get_category_link($cat->term_id); ?>">
-                        <?php echo $cat->description; ?>
-                    </a>
-                    <img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" width="100"/>
+            <?php foreach (get_categories($args) as $key => $cat) : ?>
+            <?php if ($key == 3) { echo '</div><div class="row">'; } ?>
+            <div class="col-md-4">
+                <div class="panel panel-success">
+                    <h5 class="panel-heading">
+                        <?php echo $cat->cat_name; ?>
+                    </h5>
+                    <div class="panel-body path-body">
+                        <a href="<?php echo get_category_link($cat->term_id); ?>">
+                            <?php echo $cat->description; ?>
+                        </a>
+                        <a href="<?php echo get_category_link($cat->term_id); ?>">
+                            <img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" width="100"/>
+                        </a>
+                    </div>
                 </div>
             </div>
+            <?php endforeach; ?>
         </div>
-        <?php endforeach; ?>
     </div>
 </div>
 <?php
