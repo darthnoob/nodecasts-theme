8149019e066d00fa042caeac34ac11787b489624
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-17 17:20:40 -0500

    design for list groups on category pages

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -9,28 +9,19 @@
 
 ?>
 
-<li class="list-group-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php
-        if ( is_single() ) {
-            the_title( '<h1 class="entry-title">', '</h1>' );
-        } else {
-            the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
-        }
+<?php the_title( '<a class="list-group-item" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' ); ?>
 
-    ?>
+<span class="entry-content">
+<?php
+    the_excerpt( sprintf(
+	/* translators: %s: Name of current post. */
+	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nodecasts' ), array( 'span' => array( 'class' => array() ) ) ),
+	the_title( '<span class="screen-reader-text">"', '"</span>', false )
+    ) );
 
-    <div class="entry-content">
-        <?php
-            the_excerpt( sprintf(
-                /* translators: %s: Name of current post. */
-                wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nodecasts' ), array( 'span' => array( 'class' => array() ) ) ),
-                the_title( '<span class="screen-reader-text">"', '"</span>', false )
-            ) );
-
-            wp_link_pages( array(
-                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nodecasts' ),
-                'after'  => '</div>',
-            ) );
-        ?>
-    </div><!-- .entry-content -->
-</li>
+    wp_link_pages( array(
+	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nodecasts' ),
+	'after'  => '</div>',
+    ) );
+?>
+</span><!-- .entry-content -->
